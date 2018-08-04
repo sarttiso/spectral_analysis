a3c260ee7bcc51a3b3af603fb3e1e8995ecd6341
Adrian Tasistro-Hart <adrianraph@gmail.com>
2018-08-04 12:45:18 +0200

    first commit

---
+++ pchavegram.m
@@ -0,0 +1,106 @@
+% This function computes the multi-taper spectrogram of a given signal.
+% IN:
+% y: signal
+% nw: time-halfbandwidth product
+% window: size of window in samples for sliding window
+% noverlap: size of overlap in samples
+% f: frequencies
+% fs: smapling frequency
+% ax: handle to axis in which to plot
+% 
+% TO DO:
+% - unfinished totally, just a copy of mtgram right now
+
+function [pxx,w,t] = mtgram(y,nw,window,varargin)
+
+%% parse
+parser = inputParser;
+addRequired(parser,'y',@isnumeric);
+addRequired(parser,'nw',@isscalar);
+addRequired(parser,'window',@isscalar);
+addOptional(parser,'noverlap',[],@isscalar);
+addOptional(parser,'f',[],@isnumeric);
+addOptional(parser,'fs',1,@isscalar);
+addOptional(parser,'axis',[],@ishandle);
+
+parse(parser,y,nw,window,varargin{:});
+
+y = parser.Results.y;
+nw = parser.Results.nw;
+window = parser.Results.window;
+noverlap = parser.Results.noverlap;
+f = parser.Results.f;
+fs = parser.Results.fs;
+ax = parser.Results.axis;
+    
+%% set dynamic defaults and validate
+
+% if only one column of data, make sure its a column
+if size(y,1) == 1
+    y = y';
+end
+
+% number of data points
+n = size(y,1);
+% make sure that window is smaller than the length of the data
+assert(window < n,'window is larger than length of data')
+% make window an integer
+window = double(window);
+
+% if no value for overlp, make 50%
+if isempty(noverlap)
+    noverlap = window/2;
+% otherwise make sure that overlap is less than the window
+else
+    assert(noverlap < window, 'overlap must be less than window length')
+end
+
+% set default number of frequencies
+if isempty(f)
+    % Frequency axis increment is the sampling frequency divided by number of
+    % samples
+    fi = fs/window;
+    % Frequency axis goes from 0 to 1/2 the sampling frequency
+    f = 0:fi:fs/2;
+end
+
+% recalculate overlap given discrete number of slides and given window
+% width
+nslides = floor((n-noverlap)/(window-noverlap));  % number of slides
+noverlap = ceil((nslides*window-n)/(nslides-1));
+
+%% window and compute psds
+Y = zeros(window,nslides);
+% do all but last windows
+for j = 1:nslides-1
+    lidx = round( (j-1)*(window-noverlap)+1 );
+    ridx = round( j*window-(j-1)*(noverlap) );
+    Y(:,j) = detrend(y(lidx:ridx));
+end
+% do last window
+Y(:,end) = detrend(y(end-window+1:end));
+% compute multitaper estimates
+[pxx,w] = pmtm(Y,nw,f,fs);
+% get time axis
+dt = 1/fs;
+tw = (window-noverlap) * dt; % spacing of time axis
+t1 = window/2 * dt;
+t2 = (n - window/2) * dt;
+t = t1:tw:t2;
+t(end) = t2;
+
+%% plot
+if isempty(ax)
+    figure
+else
+    axes(ax)
+end
+surf(w,t,10*log10(abs(pxx')+eps),'edgecolor','none')
+view(2)
+xlim([min(f) max(f)])
+ylim([min(t) max(t)])
+set(gca,'xscale','log')
+
+colormap(jet)
+
+end
---
+++ prewhiten.m
@@ -0,0 +1,63 @@
+% This function prewhitens a time series by fitting autoregressive
+% coefficients describing an AR(p) process such that the process has a
+% spectrum that closely matches the spectrum of the time series. As
+% described in Percival and Walden (Ch 11), these coefficients are then
+% used to compute a whitened time series: 
+% X(t) = X(t-1)*a(1) + ... + X(t-p)*a(p) + W(t), t = 1...n 
+% where X(t) is the time series we observe and W(t) is the whitenened time 
+% series we can recover by applying the coefficients "in reverse", allowing
+% us to recover n-p samples of W(t).
+% The current implementation uses arburg to estimate the lag coefficients
+% of the given process.
+% 
+% IN:
+% ts: input time series. must be a vector in the current implementation
+% 'p': (default 4) order of the AR(p) process to fit
+%
+% OUT:
+% ws: whitened time series
+%
+% TO DO:
+% - could be generalized to prewhitening by any sort of autoregressive
+%   process; i.e. we could force prewhitening by a pink noise process.
+%   Might consider making particular functions or need to think of a
+%   general way to implement prewhitening by restricting the set of
+%   coefficients a(1)...a(p)
+%
+% Adrian Tasistro-Hart, adrianraph-at-gmail.com, 04.08.2018
+
+function ws = prewhiten(ts,varargin)
+
+% parse inputs
+parser = inputParser;
+validScalarPosNum = @(x) isnumeric(x) && isscalar(x) && (x > 0);
+addRequired(parser,'ts',@isnumeric)
+addParameter(parser,'p',4,validScalarPosNum);
+
+parse(parser,ts,varargin{:})
+
+ts = parser.Results.ts;
+p = parser.Results.p;
+
+% number of samples
+n = length(ts);
+% make sure have enough samples
+assert(n>p,'need enough samples for given order')
+
+% fit AR(p) process to time series
+ab = arburg(ts,p);
+a = -ab(2:end);
+% make column
+a = a(:);
+
+% with AR(p) parameters, subtract weighted observations from ts to generate
+% ws
+ws = zeros(n,1);
+for ii = n:-1:p+1
+    ws(ii) = ts(ii) - sum(flipud(a).*ts(ii-p:ii-1));
+end
+% truncate ws
+ws = ws(p+1:end);
+
+
+end

63a20e6590efa4673fa5f7fcaa0a07e82c7cc219
Adrian Tasistro-Hart <adrianraph@gmail.com>
2018-08-18 16:55:05 +0200

    bug fixes and added more labeling parameters

--- labelpeak.m
+++ labelpeak.m
@@ -1,20 +1,26 @@
-% This function adds a vertical line and label a peak on a power spectrum.
+% This function adds a vertical line and labels a peak on a power spectrum
+% in terms of that peak's period.
+%
 % IN:
 % f: Frequency to label
 % yloc: y-coordinate of label
-% 'align': (default 'right') horizontal alignment of text (left or right)
+% 'align': (default 'right') horizontal alignment of text ('left', 'center'
+%   or 'right')
 % 'stdf': (default 0) uncertainty in frequency, 1 standard deviation 
 % 'fontsize': (default 9)
+% 'plotline': (default false) plot a vertical line intersecting the peak
 %
 % OUT:
-% h: handle of line
+% htxt: handle to the label text
+% hln: handle of line, if line is requested
 %
 % TO DO:
 % - allow user to specify precision of labels
+% - allow user to 
 %
 % Adrian Tasistro-Hart, adrianraph-at-gmail.com, 18.08.2018
 
-function h = labelpeak(f,yloc,varargin)
+function [htxt,hln] = labelpeak(f,yloc,varargin)
 
 % parse inputs
 parser = inputParser;
@@ -24,6 +30,7 @@ addRequired(parser,'yloc',@isscalar)
 addParameter(parser,'align','right',@ischar)
 addParameter(parser,'stdf',0,validScalarPosNum)
 addParameter(parser,'fontsize',9,validScalarPosNum)
+addParameter(parser,'plotline',0,@(x) x==1 || x==0)
 
 parse(parser,f,yloc,varargin{:})
 
@@ -32,21 +39,25 @@ yloc = parser.Results.yloc;
 aln = parser.Results.align;
 stdf = parser.Results.stdf;
 fontsize = parser.Results.fontsize;
+plotline = parser.Results.plotline;
 
 % validate alignment
-aln = validatestring(aln,{'left','right'});
+aln = validatestring(aln,{'left','right','center'});
 
 % formulate text
 if stdf == 0
     txt = sprintf('%1.0f',1/f);
 else
     txt = sprintf('%1.0f^{+%1.1f}_{-%1.1f}',...
-        1/f, abs(1/f - 1/(f+2*stdf)), abs(1/f - 1/(f-2*stdf)));
+        1/f, abs(1/f - 1/(f-2*stdf)), abs(1/f - 1/(f+2*stdf)));
 end
 
-h = vline(f,'k-');
-text(f,yloc,txt,'fontsize',fontsize,...
+if plotline
+    hln = vline(f,'k-');
+end
+
+htxt = text(f,yloc,txt,'fontsize',fontsize,...
     'HorizontalAlignment',aln,'VerticalAlignment','bottom',...
-    'BackgroundColor','none','Margin',1)
+    'BackgroundColor','none','Margin',1);
 
 end
